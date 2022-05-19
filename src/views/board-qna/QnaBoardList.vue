ec06a4877808492a45c6b97f7d6346436bede94a
mingyu <mingyu1218@naver.com>
2022-05-20 02:12:16 +0900

    feature: page navigation 구현

--- src/views/board-qna/QnaBoardList.vue
+++ src/views/board-qna/QnaBoardList.vue
@@ -3,18 +3,50 @@
     <button class="btn btn-primary" @click="moveWrite()">글쓰기</button>
 
     <!-- 게시판 element 시작 -->
-    <div
-      class="notice-post"
-      :id="item.bno"
-      v-for="(item, index) in qnaBoard"
-      :key="index"
-      @click="moveDetail(item.bNo)"
-    >
-      <div class="notice-title">{{ item.bTitle }}</div>
-      <div class="notice-date">{{ item.bWriteDate }}</div>
-      <div class="notice-writer">작성자 : {{ item.bWriterName }}</div>
-    </div>
+    <section class="board_container mb-5">
+      <div
+        class="notice-post"
+        :id="item.bno"
+        v-for="(item, index) in qnaBoard"
+        :key="index"
+        @click="moveDetail(item.bNo)"
+      >
+        <div class="notice-title">{{ item.bTitle }}</div>
+        <div class="notice-date">{{ item.bWriteDate }}</div>
+        <div class="notice-writer">작성자 : {{ item.bWriterName }}</div>
+      </div>
+    </section>
     <!-- 게시판 element 끝 -->
+
+    <!-- Page Navigation 시작 -->
+    <div align="center" class="page_navigation row mb-5">
+      <div
+        class="page_element col-1"
+        style="cursor: pointer"
+        v-if="page - 1"
+        @click="moveBeforePage()"
+      >
+        이전
+      </div>
+      <div class="page_element col-1" v-else></div>
+      <div
+        class="page_element col-1"
+        v-for="(item, index) in pageNavigation"
+        :key="index"
+        v-html="item"
+        @click="movePage(item)"
+      ></div>
+      <div
+        class="page_element col-1"
+        style="cursor: pointer"
+        v-if="page !== totalPage"
+        @click="moveAfterPage()"
+      >
+        다음
+      </div>
+      <div class="page_element col-1" v-else></div>
+    </div>
+    <!-- Page Navigation 끝 -->
   </div>
 </template>
 
@@ -25,30 +57,59 @@ export default {
   name: "QnaBoardList",
   data() {
     return {
-      // 더미 데이터 (axios해서 받아올 것!)
-      qnaBoard: [],
+      page: 1, // 현재 페이지 (초기 기본값 1)
+      totalPage: 0,
+      qnaBoard: [], // 게시글 목록
+      pageNavigation: [], // 페이지 내비게이션 목록
     };
   },
   created() {
-    // QnaBoard를 axios로 받아옴
-    http
-      .get(`/qna-board/getList`)
-      .then(({ data }) => {
-        this.qnaBoard = data;
-      })
-      .then(() => {
-        // 게시글을 bNo 역순으로 정렬
-        this.qnaBoard.sort((a, b) => {
-          return b.bNo - a.bNo;
-        });
-      });
+    this.getArticleList();
   },
   methods: {
+    getArticleList() {
+      http
+        .get(`/qna-board/getList`, {
+          params: {
+            page: this.page,
+          },
+        })
+        .then(({ data }) => {
+          this.page = data.result.currentPage;
+          this.qnaBoard = data.result.articleList;
+          this.totalPage = data.result.totalPage;
+          this.pageNavigation = [];
+          for (let i = data.result.startPage; i <= data.result.endPage; i++) {
+            if (this.page === i) {
+              this.pageNavigation.push(
+                "<strong class='bg-warning'>" + i + "</strong>"
+              );
+            } else {
+              this.pageNavigation.push(i);
+            }
+          }
+        });
+    },
     moveDetail(bNo) {
       this.$router.push({ name: "QnaBoardDetail", query: { bno: bNo } });
     },
     moveWrite() {
       this.$router.push({ name: "QnaBoardCreate" });
+    },
+    pageNaviClickHandler() {
+      console.log("page navigation");
+    },
+    moveBeforePage() {
+      this.page -= 1;
+      this.getArticleList();
+    },
+    moveAfterPage() {
+      this.page += 1;
+      this.getArticleList();
+    },
+    movePage(pg) {
+      this.page = pg;
+      this.getArticleList();
     },
   },
 };
@@ -70,5 +131,9 @@ export default {
   font-size: 14px;
   font-weight: bold;
   color: #848484;
+}
+.page_element {
+  border: 1px solid blue;
+  color: blue;
 }
 </style>
