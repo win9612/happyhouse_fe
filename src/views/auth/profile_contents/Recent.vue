4abb755de5ca6f39fb8b7ef062d817e9e1294302
park sang hyeop <48915018+parksanghyup@users.noreply.github.com>
2022-05-24 04:17:31 +0900

    프로필 UI 껍데기완료

--- src/views/auth/profile_contents/Recent.vue
+++ src/views/auth/profile_contents/Recent.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>recent</div>
+  <div class="likeList-contents-container">
+    <div class="like-post">
+      <div class="notice-title">은마아파트</div>
+      <div class="notice-dong">대치동</div>
+    </div>
+    <div class="like-post">
+      <div class="notice-title">은마아파트</div>
+      <div class="notice-dong">대치동</div>
+    </div>
+    <div class="like-post">
+      <div class="notice-title">은마아파트</div>
+      <div class="notice-dong">대치동</div>
+    </div>
+    <div class="like-post">
+      <div class="notice-title">은마아파트</div>
+      <div class="notice-dong">대치동</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +24,19 @@ export default {
   name: "Recent",
 };
 </script>
+
+<style>
+.likeList-contents-container {
+  margin-top: 40px;
+}
+.like-post {
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  border: 1px solid #e0e0e0;
+  border-radius: 7px;
+  box-sizing: border-box;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
