46c713b40d61f927699b62a49fdc757a68726c6f
mingyu <mingyu1218@naver.com>
2022-05-24 02:03:32 +0900

    feature: 메인페이지 네이버뉴스 크롤링 구현

--- src/views/home/IntroService.vue
+++ src/views/home/IntroService.vue
@@ -1,22 +1,66 @@
 <template>
   <section class="section2">
-    <div class="intro-service-title">추천 매물 들어갈 예정</div>
+    <div class="intro-service-title">부동산 뉴스</div>
 
-    <div class="card-container row">
-      <div class="col service-card">카드1</div>
-      <div class="col service-card">카드2</div>
-      <div class="col service-card">카드3</div>
+    <div v-if="news.length > 0">
+      <carousel
+        class="card-container row bg-light"
+        :autoplay="true"
+        :nav="true"
+      >
+        <div
+          class="col service-card m-3 p-3"
+          v-for="(item, index) in news"
+          :key="index"
+        >
+          <a class="news_box" :href="item.link" target="_blank">
+            <h2 class="news_title mb-4" v-html="item.title"></h2>
+            <div class="news_description" v-html="item.description"></div
+          ></a>
+        </div>
+      </carousel>
     </div>
   </section>
 </template>
 
 <script>
+import http from "../../api/http-common";
+import carousel from "vue-owl-carousel";
 export default {
   name: "IntroService",
+  components: {
+    carousel,
+  },
+  data() {
+    return {
+      keyword: "부동산",
+      news: [],
+    };
+  },
+  created() {
+    http
+      .get(`/naver/news/${this.keyword}`)
+      .then(({ data }) => {
+        this.news = data.items;
+        let d1 = Date.parse(this.news[0].pubDate);
+        let d2 = new Date(d1);
+        console.log(d2.getMonth());
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
 
 <style>
+.news_box {
+  color: black;
+  text-decoration: none;
+}
+.news_title {
+  font-weight: bold;
+}
 .section2 {
   background-color: #fff;
   padding: 94px 0px 100px;
